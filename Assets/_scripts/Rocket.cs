feda46c5b72d1e44710c8eadc2e4f13550e1f89d
Ike <j.ike.m.bryce@gmail.com>
2018-08-14 15:14:56 +1200

    ch 56
    invoke and  level load slowdown

--- Assets/_scripts/Rocket.cs
+++ Assets/_scripts/Rocket.cs
@@ -9,7 +9,10 @@ public class Rocket : MonoBehaviour
     [SerializeField]float rcsThrust = 100f;
     [SerializeField]float Boosters = 100f;
 
-    
+
+    enum State {Alive, Dying, Trancending }
+    State state = State.Alive;
+
 
     void Start()
     {
@@ -24,12 +27,17 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
+        if (state == State.Alive)
+        {
         thrusters();
         rotators();
-
+        }
+        
     }
     private void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive){ return;}//ignore collisions after death
+        
         print("Collideded");
         switch (collision.gameObject.tag)
         {
@@ -38,28 +46,44 @@ public class Rocket : MonoBehaviour
                 break;
             case "Finish":
                 print("Fin");
-                SceneManager.LoadScene(1);
+                state = State.Trancending;
+                Invoke("LoadNextScene",1f);
                 break;
             default:
                 print("ded");
-                SceneManager.LoadScene(0);
+                state = State.Dying;
+                Invoke("ReloadLevel_1",1f);
+
                 break;
 
         }
+    }
+
+    private void ReloadLevel_1()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
     }
 
     private void thrusters()
     {
         if (Input.GetKey(KeyCode.Space))
         {
+                      
             print("thrusters engage");
             float BoostThis = Boosters * Time.deltaTime;
 
             rigidBody.AddRelativeForce(Vector3.up * BoostThis);
             if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+                {
+                    audioSource.Play();
+                }                
+            
+               
         }
         else { audioSource.Stop(); }
     }
